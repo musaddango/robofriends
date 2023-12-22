c25e5aec1872082d8f785694eb03223eab4b87a8
Musa Dango <musaddangoWgmail.com>
2023-12-22 15:28:58 +0100

    Adjusted the robots to be fetched directly from the API

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import CardList from '../Components/cardList.js';
+import SearchBox from '../Components/SearchBox.js';
+// import { robots } from '../Components/robots.js';
+import { useState, useEffect } from 'react';
+import Scroll from '../Components/Scroll.js';
+import ErrorBoundry from '../Components/ErrorBoundry.js';
+import './App.css';
+import 'tachyons';
+
+
+// Functional Method using react hooks
+
+const App = () => {
+	const [robots, setRobots] = useState([]);
+	const [searchField, setSearchField] = useState("");
+
+	useEffect(()=> {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then((response)=> response.json())
+		.then((users)=> setRobots(users));
+	}, [])
+
+	const onSearchChange = event=>{
+		setSearchField(event.target.value);
+	}
+
+	
+		const filteredRobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
+		});
+
+		console.log(robots, searchField);
+
+		return !robots.length ? 
+			<h1 className='tc'>Loading</h1>
+			: 
+			(
+				<div className='tc scroll'>
+					<div className='Scroll'>
+						<h1 className='tc f1 sega'>RoboFriends</h1>
+						<SearchBox searchChange= {onSearchChange} />
+					</div>
+					<ErrorBoundry>
+						<Scroll>
+							< CardList robots={filteredRobots}/>
+						</Scroll>
+					</ErrorBoundry>
+				</div>
+			);
+	}
+
+
+
+export default App;
