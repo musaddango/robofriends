c6370eb6f561ca114284fa9f151a96564d497ed6
Musa Dango <musaddangoWgmail.com>
2023-12-26 09:52:34 +0100

    Redux set-up

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,10 +1,7 @@
 const Scroll = (props) => {
   return (
-<<<<<<< HEAD
     <div style={{ overflow: 'scroll', border: '5px solid black', height: '400px'}}>
-=======
-    <div style={{ overflow: 'scroll', border: '5px solid black', height: '600px'}}>
->>>>>>> 733d48b42de2f04628c3fc9ede0bd15782ff1684
+
       {props.children}
     </div>
   );
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,25 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 // const mapStateToProp = (state)=>({
 //   searchfield: state.SearchBox,
 // })
 
-=======
-import CardList from '../Components/cardList.js';
-import SearchBox from '../Components/SearchBox.js';
-import { useState, useEffect } from 'react';
-import Scroll from '../Components/Scroll.js';
-import ErrorBoundry from '../Components/ErrorBoundry.js';
-import './App.css';
-import 'tachyons';
->>>>>>> 733d48b42de2f04628c3fc9ede0bd15782ff1684
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
 
-<<<<<<< HEAD
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
@@ -31,15 +28,11 @@ import 'tachyons';
         throw new Error(`Error: ${err}`)
       })
   }
-=======
-// Functional Method using react hooks
->>>>>>> 733d48b42de2f04628c3fc9ede0bd15782ff1684
 
-const App = () => {
-	const [robots, setRobots] = useState([]);
-	const [searchField, setSearchField] = useState("");
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
 
-<<<<<<< HEAD
   render() {
     const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot =>{
@@ -60,43 +53,5 @@ const App = () => {
       );
   }
 }
-=======
-	useEffect(()=> {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then((response)=> response.json())
-		.then((users)=> setRobots(users));
-	}, [])
->>>>>>> 733d48b42de2f04628c3fc9ede0bd15782ff1684
-
-	const onSearchChange = event=>{
-		setSearchField(event.target.value);
-	}
-
-	
-		const filteredRobots = robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(searchField.toLowerCase());
-		});
-
-		console.log(robots, searchField);
-
-		return !robots.length ? 
-			<h1 className='tc'>Loading</h1>
-			: 
-			(
-				<div className='tc scroll'>
-					<div className='Scroll'>
-						<h1 className='tc f1 sega'>RoboFriends</h1>
-						<SearchBox searchChange= {onSearchChange} />
-					</div>
-					<ErrorBoundry>
-						<Scroll>
-							< CardList robots={filteredRobots}/>
-						</Scroll>
-					</ErrorBoundry>
-				</div>
-			);
-	}
-
-
 
 export default App;
