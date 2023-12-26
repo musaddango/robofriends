751af8d5d258348b1612157c77e5c264f7216399
Musa Dango <musaddangoWgmail.com>
2023-12-26 23:39:11 +0100

    before the thunk middleware implementation

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -13,4 +13,5 @@ export const setSearchField = text => ({
 export const requestRobots = (dispatch) =>{
     dispatch({type: REQUEST_ROBOTS_PENDING});
     fetch('https://jsonplaceholder.typicode.com/users');
+    
 }
