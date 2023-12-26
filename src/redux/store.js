e6ecad9c6a20f01dce79a0509aafad0a8e67ecc3
Musa Dango <musaddangoWgmail.com>
2023-12-26 22:02:46 +0100

    before the thunk middleware implementation

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import { reducer } from './reducers';
+import { thunk } from 'redux-thunk';
+import logger from 'redux-logger';
+import { applyMiddleware } from 'redux';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 export default store;
