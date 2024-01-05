02bff6d4ed48d439929738a0426c4eff8328804b
Musa Dango <musaddangoWgmail.com>
2024-01-05 10:12:36 +0100

    First test commit

---
+++ src/components/Card.test.js
@@ -0,0 +1,7 @@
+import { shallow } from 'enzyme';
+import Card from './Card';
+
+it('expect to render Card component', ()=>{
+    expect(shallow(<Card />)).toMatchSnapshot();
+    console.log(shallow(<Card />).debug());
+})
